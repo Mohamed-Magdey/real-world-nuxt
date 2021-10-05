c0672d46c5310cd35dd8abeef2d576e808b9f8cd
mohamed <ragabmohame6@gmail.com>
2021-10-05 17:19:47 +0200

    Integrate index with db.json

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    EventCard: () => import('~/components/EventCard.vue'),
+  },
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3000/events')
+      return {
+        events: data,
+      }
+    } catch (err) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again.',
+      })
+    }
+  },
   head() {
     return {
       title: 'Event Listing',
