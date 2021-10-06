0a12997e4d6b0f5683b98b512ff3e8910c98d2bd
mohamed <ragabmohame6@gmail.com>
2021-10-06 16:37:10 +0200

    Static generate

---
+++ docs/_nuxt/static/1633530787/event/create/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Freal-world-nuxt\u002F_nuxt\u002Fstatic\u002F1633530787",layout:"default",error:a,state:{events:{events:[],event:{}}},serverRendered:true,routePath:"\u002Fevent\u002Fcreate",config:{_app:{basePath:"\u002Freal-world-nuxt\u002F",assetsPath:"\u002Freal-world-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
