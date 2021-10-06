d47bb57023eade025812a3d8bb395ec14570a522
mohamed <ragabmohame6@gmail.com>
2021-10-06 14:40:20 +0200

    Add base to router in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/real-world-nuxt/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -8,7 +11,6 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    base: { href: '/real-world-nuxt/' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
