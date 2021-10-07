ad7aedc8ae646e0d4ee0712c153bb2925aae42de
mohamed <ragabmohame6@gmail.com>
2021-10-07 19:20:52 +0200

    Ready to deploy

---
+++ docs/_nuxt/static/1633627048/event/4583797/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/event/4583797", (function(a,b){return {data:[{}],fetch:{},mutations:[["events\u002FSET_EVENT",{id:4583797,user:{id:a,name:b},category:"animal welfare",organizer:{id:a,name:b},title:"Puppy Parade",description:"Yay pups!",location:"Puptown ",date:"2019-02-02T23:27:00.000Z",time:"1:00",attendees:[]}]]}}("abc123","Adam")));
