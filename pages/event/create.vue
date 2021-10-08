3e7425ba6b8e08043b2f66689a0eb6683ccd46b1
mohamed <ragabmohame6@gmail.com>
2021-10-08 16:14:56 +0200

    Refactor create event with integration

--- pages/event/create.vue
+++ pages/event/create.vue
@@ -1,11 +1,66 @@
 <template>
   <div>
     <h1>Create an Event</h1>
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <date-picker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    const times = []
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      event: this.createFreshEventObject(),
+      times,
+      categories: this.$store.state.categories,
+    }
+  },
   head() {
     return {
       title: 'Create an Event',
@@ -18,5 +73,38 @@ export default {
       ],
     }
   },
+  methods: {
+    createEvent() {
+      this.$store.dispatch('events/createEvent', this.event).then(() => {
+        this.$router.push({
+          name: 'index',
+        })
+        this.event = this.createFreshEventObject()
+      })
+    },
+    createFreshEventObject() {
+      const id = Math.floor(Math.random() * 10000000)
+      return {
+        id,
+        category: '',
+        organizer: {
+          id: 'abc123',
+          name: 'Adam',
+        },
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: [],
+      }
+    },
+  },
 }
 </script>
+
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
