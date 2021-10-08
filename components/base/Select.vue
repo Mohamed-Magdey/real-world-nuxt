5b795456cf69ffccb89684d734b4b12d45a5784b
mohamed <ragabmohame6@gmail.com>
2021-10-08 18:46:25 +0200

    Create global select component

---
+++ components/base/Select.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select :value="value" v-bind="$attrs" @input="updateValue">
+      <option v-for="option in options" :key="option">{{ option }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    // eslint-disable-next-line vue/require-default-prop
+    value: [String, Number],
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    },
+  },
+}
+</script>
