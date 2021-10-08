f80a54bb873b5c9707a40b70e26bbaed4cd2eaf2
mohamed <ragabmohame6@gmail.com>
2021-10-08 18:39:32 +0200

    Create global input component

---
+++ components/base/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" v-bind="$attrs" @input="updateValue" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    // eslint-disable-next-line vue/require-default-prop
+    value: [String, Number],
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    },
+  },
+}
+</script>
