176f3798500a8fee3d61ae3888e9054dfdd0d508
mohamed <ragabmohame6@gmail.com>
2021-10-11 19:04:45 +0200

    New build

--- docs/_nuxt/static/1633715395/event/4583797/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/event/4583797", (function(a,b){return {data:[{}],fetch:{"data-v-2e2a9cee:0":{}},mutations:[["events\u002FSET_EVENT",{id:4583797,user:{id:a,name:b},category:"animal welfare",organizer:{id:a,name:b},title:"Puppy Parade",description:"Yay pups!",location:"Puptown ",date:"2019-02-02T23:27:00.000Z",time:"1:00",attendees:[]}]]}}("abc123","Adam")));
---
+++ docs/_nuxt/static/1633971803/event/8419988/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/event/8419988", (function(a,b){return {data:[{}],fetch:{"data-v-2de81574:0":{}},mutations:[["events\u002FSET_EVENT",{id:8419988,user:{id:a,name:b},category:"animal welfare",organizer:{id:a,name:b},title:"Kitty Cluster",description:"Yay cats!",location:"Catlandia",date:"2019-01-31T22:09:00.000Z",time:"7:00",attendees:[]}]]}}("abc123","Adam")));
