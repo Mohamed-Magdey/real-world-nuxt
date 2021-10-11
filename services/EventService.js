322d770d138379b60968fa014f45aa5673023081
mohamed <ragabmohame6@gmail.com>
2021-10-11 19:01:30 +0200

    Refactor service

--- services/EventService.js
+++ services/EventService.js
@@ -15,7 +15,7 @@ export default {
     return apiClient.get(`/events?_limit=${perPage}&_page=${page}`)
   },
   getEvent(id) {
-    return apiClient.get(`/events/${id}`)
+    return apiClient.get('/events/' + id)
   },
   postEvent(event) {
     return apiClient.post("/events", event);
