e9bf37f7c5f78ff7813d14990ce6c3756f5663c9
mohamed <ragabmohame6@gmail.com>
2021-10-11 12:43:49 +0200

    Add validations for create form

--- pages/event/create.vue
+++ pages/event/create.vue
@@ -4,53 +4,113 @@
     <form @submit.prevent="createEvent">
       <BaseSelect
         v-model="event.category"
-        label="Select a category"
+        label="Select a category*"
         :options="categories"
+        :class="{ error: $v.event.category.$error }"
+        @blur="$v.event.category.$touch()"
       />
+      <template v-if="$v.event.category.$error">
+        <p v-if="!$v.event.category.required" class="errorMessage">
+          Category is required.
+        </p>
+      </template>
+
       <h3>Name & describe your event</h3>
       <BaseInput
         v-model="event.title"
-        label="Title"
+        label="Title*"
         type="text"
         placeholder="Title"
         class="field"
+        :class="{ error: $v.event.title.$error }"
+        @blur="$v.event.title.$touch()"
       />
+      <template v-if="$v.event.title.$error">
+        <p v-if="!$v.event.title.required" class="errorMessage">
+          Title is required.
+        </p>
+      </template>
+
       <BaseInput
         v-model="event.description"
         type="text"
-        label="Description"
+        label="Description*"
         placeholder="Description"
         class="field"
+        :class="{ error: $v.event.description.$error }"
+        @blur="$v.event.description.$touch()"
       />
+      <template v-if="$v.event.description.$error">
+        <p v-if="!$v.event.description.required" class="errorMessage">
+          Description is required.
+        </p>
+      </template>
+
       <h3>Where is your event?</h3>
       <BaseInput
         v-model="event.location"
         type="text"
-        label="Location"
+        label="Location*"
         placeholder="Location"
         class="field"
+        :class="{ error: $v.event.location.$error }"
+        @blur="$v.event.location.$touch()"
       />
+      <template v-if="$v.event.location.$error">
+        <p v-if="!$v.event.location.required" class="errorMessage">
+          Location is required.
+        </p>
+      </template>
+
       <h3>When is your event?</h3>
       <div class="field">
-        <label>Date</label>
+        <label>Date*</label>
         <client-only>
-          <date-picker v-model="event.date" placeholder="Select a date" />
+          <date-picker
+            v-model="event.date"
+            placeholder="Select a date"
+            :input-class="{ error: $v.event.date.$error }"
+            @focusin.native="$v.event.date.$touch()"
+          />
         </client-only>
       </div>
+      <template v-if="$v.event.date.$error">
+        <p v-if="!$v.event.date.required" class="errorMessage">
+          Date is required.
+        </p>
+      </template>
+
       <BaseSelect
         v-model="event.time"
-        label="Select a time"
+        label="Select a time*"
         :options="times"
         class="field"
+        :class="{ error: $v.event.time.$error }"
+        @blur="$v.event.time.$touch()"
       />
-      <BaseButton type="submit" class="button -fill-gradient">
+      <template v-if="$v.event.time.$error">
+        <p v-if="!$v.event.time.required" class="errorMessage">
+          Time is required.
+        </p>
+      </template>
+
+      <BaseButton
+        type="submit"
+        class="button -fill-gradient"
+        :disabled="$v.$anyError"
+      >
         Submit
       </BaseButton>
+      <p v-if="$v.$anyError" class="errorMessage">
+        Please fill out the required field(s).
+      </p>
     </form>
   </div>
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators'
+
 export default {
   data() {
     const times = []
@@ -75,14 +135,27 @@ export default {
       ],
     }
   },
+  validations: {
+    event: {
+      category: { required },
+      title: { required },
+      description: { required },
+      location: { required },
+      date: { required },
+      time: { required },
+    },
+  },
   methods: {
     createEvent() {
-      this.$store.dispatch('events/createEvent', this.event).then(() => {
-        this.$router.push({
-          name: 'index',
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        this.$store.dispatch('events/createEvent', this.event).then(() => {
+          this.$router.push({
+            name: 'index',
+          })
+          this.event = this.createFreshEventObject()
         })
-        this.event = this.createFreshEventObject()
-      })
+      }
     },
     createFreshEventObject() {
       const id = Math.floor(Math.random() * 10000000)
